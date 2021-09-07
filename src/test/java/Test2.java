2246f6f69dad13a07da6f3a8e4e3248234639c60
dee7050 <tdk7011@ro.ru>
2021-09-07 19:12:00 +0300

    Add files

---
+++ src/test/java/Test2.java
@@ -0,0 +1,13 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Test2 {
+
+}
